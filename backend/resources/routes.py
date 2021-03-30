99bbe58334b3fe612ae02e49c32d65c71bb9e829
quinh <quinh@rikkeisoft.com>
2021-03-30 15:19:21 +0700

    Flask with mongo

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -1,16 +1,9 @@
-from .resources import BSTimelineAPI, BSTimelineCollection
-from .bs_20_rt import BS_20
-from .bsr import BSR
+from .order import *
 
 
 def initialize_routes(api):
-    api.add_resource(BSTimelineCollection, '/api/bstimeline')
-    api.add_resource(BSTimelineAPI, '/api/bstimeline/<id>')
-
-    api.add_resource(BS_20, '/api/bs20')
-
-    api.add_resource(BSR, '/api/bsr')
-
+    api.add_resource(OrdersApi, '/api/orders')
+    api.add_resource(OrderApi, '/api/order')
 
 
 
