2ba3dec0c5bc4129985281ed54f480d4c43218a4
Quinh <quinh>
2021-09-04 14:52:02 +0700

    update from develop

--- backend/database/db.py
+++ backend/database/db.py
@@ -3,12 +3,27 @@ import simplejson as json
 from sqlalchemy.orm import *
 from sqlalchemy import *
 from envi import *
+import sys, inspect
+import database.models as mymodel
+from sqlalchemy.orm.decl_api import DeclarativeMeta
 
+def gen_engine():
+    return create_engine(f'mysql+mysqldb://{SQL_SERVER.user}:{SQL_SERVER.password}@{SQL_SERVER.host}/{SQL_SERVER.db}?charset=utf8')
 
 def ConnectDB():
-    engine = create_engine(f'mysql+mysqldb://{SQL_SERVER.user}:{SQL_SERVER.password}@{SQL_SERVER.host}/{SQL_SERVER.db}?charset=utf8')
+    engine = gen_engine()
     Session = sessionmaker(bind=engine)
     return Session
 
-def initialize_db(a):
-    pass
+def initialize_db(new=False):
+    models = inspect.getmembers(mymodel, inspect.isclass)
+    engine = gen_engine()
+    for model in models:
+        # print(model[1].__name__)
+        if model[1].__class__.__name__ == "DeclarativeMeta" and model[1].__name__ != "Base":
+            this_table = model[1]
+            if new:
+                this_table.__table__.drop(engine)
+                this_table.__table__.create(engine)
+            elif not engine.has_table(this_table.__tablename__):
+                this_table.__table__.create(engine)
